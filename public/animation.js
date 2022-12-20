fe5536cff469e892e329bc4be6dc7c7f9ff09e6a
jiaolyulu <ll4132@nyu.edu>
2022-12-20 10:54:45 -0500

    importing gsap issue

---
+++ public/animation.js
@@ -0,0 +1,18 @@
+
+//const gsap = require("gsap")
+import {gsap} from "../gsap"
+var tl = gsap.timeline({ smoothChildTiming: true, repeat: 0 });
+tl.fromTo(".textfloat", {scale:1},{
+  duration: 1,
+  scale: 0.8,
+  ease: "power1.inOut",
+  stagger: {
+    grid: [7, 15],
+    from: "left",
+    amount: 1.5
+  }
+});
+
+document.onmousemove = function (e) {
+  tl.progress(2. * Math.abs(e.pageX / window.innerWidth - 0.5), true);
+}
